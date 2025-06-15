3cd5950bcc08e98ebc7cf3d351bfbfed48f02417
Marta Dychała <mar754dyc@gmail.com>
2025-06-15 13:36:37 +0200

    Implement login via LinkedIn - initial implementation

--- src/main/java/com/app/solid/SecurityConfiguration.java
+++ src/main/java/com/app/solid/SecurityConfiguration.java
@@ -1,12 +1,40 @@
 package com.app.solid;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.endpoint.DefaultMapOAuth2AccessTokenResponseConverter;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
+import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -17,30 +45,100 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Autowired
+    ClientRegistrationRepository clientRegistrationRepository;
+    private static final String[] AUTH_WHITELIST = {
+            "/",
+            "/v1/api/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html"
+    };
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(req ->
-                        req.requestMatchers(
-                                        "/v1/api/**",
-                                        "/v2/api-docs",
-                                        "/v3/api-docs",
-                                        "/v3/api-docs/**",
-                                        "/swagger-resources",
-                                        "/swagger-resources/**",
-                                        "/configuration/ui",
-                                        "/configuration/security",
-                                        "/swagger-ui/**",
-                                        "/webjars/**",
-                                        "/swagger-ui.html"
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(AUTH_WHITELIST).permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(oauth -> oauth
+                                .authorizationEndpoint((authorizationEndpointConfig ->
+                                authorizationEndpointConfig.authorizationRequestResolver(
+                                        requestResolver(this.clientRegistrationRepository)
+                                ))
+                                ).tokenEndpoint(tokenEndpointConfig -> tokenEndpointConfig
+                                .accessTokenResponseClient(linkedinTokenResponseClient())
                                 )
-                                .permitAll()
-                                .anyRequest()
-                                .anonymous()
+                        .failureHandler((request, response, exception) -> {
+                            response.setStatus(401);
+                            response.getWriter().write("Authentication failed: " + exception.getMessage());
+                            response.getWriter().write(exception.toString());
+                        })
                 )
-                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
+//                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+        ;
         return http.build();
+    }
+
+    private static DefaultAuthorizationCodeTokenResponseClient linkedinTokenResponseClient() {
+        var defaultMapConverter = new DefaultMapOAuth2AccessTokenResponseConverter();
+        Converter<Map<String, Object>, OAuth2AccessTokenResponse> linkedinMapConverter = tokenResponse -> {
+            var withTokenType = new HashMap<>(tokenResponse);
+            withTokenType.put(OAuth2ParameterNames.TOKEN_TYPE, OAuth2AccessToken.TokenType.BEARER.getValue());
+            return defaultMapConverter.convert(withTokenType);
+        };
+
+        var httpConverter = new OAuth2AccessTokenResponseHttpMessageConverter();
+        httpConverter.setAccessTokenResponseConverter(linkedinMapConverter);
+
+        var restOperations = new RestTemplate(List.of(new FormHttpMessageConverter(), httpConverter));
+
+        restOperations.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
+        var client = new DefaultAuthorizationCodeTokenResponseClient();
+        client.setRestOperations(restOperations);
+        return client;
+    }
+
+    private static DefaultOAuth2AuthorizationRequestResolver requestResolver
+            (ClientRegistrationRepository clientRegistrationRepository) {
+        DefaultOAuth2AuthorizationRequestResolver requestResolver =
+                new DefaultOAuth2AuthorizationRequestResolver(clientRegistrationRepository,
+                        "/oauth2/authorization");
+        requestResolver.setAuthorizationRequestCustomizer(c ->
+                c.attributes(stringObjectMap -> stringObjectMap.remove(OidcParameterNames.NONCE))
+                        .parameters(params -> params.remove(OidcParameterNames.NONCE))
+        );
+
+
+        return requestResolver;
+    }
+
+    private static void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+        String email = (String) oauth2User.getAttributes().get("emailAddress");
+
+        String firstName = oauth2User.getAttribute("localizedFirstName");
+        String lastName = oauth2User.getAttribute("localizedLastName");
+
+        System.out.println("Authentication successful: " + authentication.getName());
+
+
+
+        request.getSession().setAttribute("email", email);
+        request.getSession().setAttribute("name", oauth2User.getAttribute("name"));
+
+        response.sendRedirect("/");
     }
 }
